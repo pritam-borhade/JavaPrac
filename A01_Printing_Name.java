3a768db99d8bc224b767f745dc6e407caf717b44
pritam-borhade <pritam.borhade@gmail.com>
2024-10-28 21:28:04 -0400

    Pritam's first commit

---
+++ A01_Printing_Name.java
@@ -0,0 +1,23 @@
+package learning;
+
+public class A01_Printing_Name {
+// Assignment no.01: Print your name vertically and Horizontally.
+
+	public static void main(String[] args) 
+	{
+		System.out.print("Printing Horizontally Name = ");
+		System.out.println("Prtiam");
+		
+		System.out.println("Printing Vertically Name");
+		System.out.println("P");
+		System.out.println("R");
+		System.out.println("I");
+		System.out.println("T");
+		System.out.println("A");
+		System.out.println("M");
+		System.out.print("How are you?");
+		System.out.println("How are you?");
+
+	}
+
+}
