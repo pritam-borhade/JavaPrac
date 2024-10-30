cc698d201e9a3c5b86ca43f138a2f55d7230973d
pritam-borhade <pritam.borhade@gmail.com>
2024-10-29 21:16:42 -0400

    Opening amazon

---
+++ Assignments/Selenium Assignments/As_01Amazon.java
@@ -0,0 +1,15 @@
+package selenium_Pract;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class As_01Amazon 
+{
+public static void main(String[] args) 
+{
+	ChromeDriver driver= new ChromeDriver();
+	driver.get("https://www.amazon.com/");
+	
+	
+}
+}
