ab50384ce715398130a78fe7cd58f79dbd3ce35f
pritam-borhade <pritam.borhade@gmail.com>
2025-01-19 12:12:10 -0500

    Create Assignment_demo_Jenkins.java
    Pract for Jenkins and Git

---
+++ Assignment_demo_Jenkins.java
@@ -0,0 +1,25 @@
+package assiggnments;
+
+public class As11_LeapYear 
+{
+	public static void main(String[] args) 
+	{
+		int ST =1990;
+		int ET=2024;
+		System.out.println("Leap year between 1990 and 2024 is below");
+		//365 days in a year || 366 in leap year
+		//2000,2020,2024-> dosent leave out remainder-> Completely divisible by 4
+		for (int year=ST; year<=ET; year++) 
+		{
+			if (year %4==0 ) 
+			{
+			System.out.println(year+" is a leap yesr");
+			}
+			else
+			{
+			System.out.println(year+" is not a leap yesr");
+			}
+		}
+	}
+
+}
